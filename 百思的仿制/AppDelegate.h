2300c71c6507c83a05b89623541c93a3cf94f75c
张精申 <123qwe@zhangjingshendemacbook-air.local>
2016-05-17 16:04:22 +0800

    Initial Commit

---
+++ 百思的仿制/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思的仿制
+//
+//  Created by 张精申 on 16/5/17.
+//  Copyright © 2016年 张精申. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思的仿制/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思的仿制
+//
+//  Created by 张精申 on 16/5/17.
+//  Copyright © 2016年 张精申. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
